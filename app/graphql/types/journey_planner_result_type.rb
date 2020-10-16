645f8e4dd7bca4778daf2d062a7b5b6598885aae
Mathew Trivett <mathew@wearesnook.com>
2020-10-16 11:31:56 +0100

    fix: files ready to start work

--- app/graphql/types/journey_planner_result_type.rb
+++ app/graphql/types/journey_planner_result_type.rb
@@ -3,5 +3,6 @@
 module Types
   class JourneyPlannerResultType < Types::BaseObject
     field :recommendedMaxAgeMinutes, Integer, null: true
+    field :stopMessages, [String, null: true], null: true
   end
 end
