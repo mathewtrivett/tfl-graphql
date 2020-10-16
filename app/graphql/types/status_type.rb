fe42c7cffbb0cf0f6c4600aed27ff1dd49655fd2
Mathew Trivett <mathew@wearesnook.com>
2020-10-16 18:15:31 +0100

    feat: Adds identifier and status types closes #36 closes #42

---
+++ app/graphql/types/status_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class StatusType < Types::BaseEnum
+    value 'UNKNOWN'
+    value 'ALL'
+    value 'OPEN'
+    value 'IN_PROGRESS'
+    value 'PLANNED'
+    value 'PLANNED___SUBJECT_TO_FEASIBILITY_AND_CONSULTATION_'
+    value 'NOT_OPEN'
+  end
+end
---
+++ spec/graphql/types/status_type_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe Types::StatusType do
+  subject { described_class }
+
+  it { is_expected.to respond_to(:values) }
+
+  it "is expected to define an ALL value" do
+    expect(subject.values).to include("ALL")
+  end
+
+  it "is expected to define an UNKNOWN value" do
+    expect(subject.values).to include("UNKNOWN")
+  end
+
+  it "is expected to define a OPEN value" do
+    expect(subject.values).to include("OPEN")
+  end
+
+  it "is expected to define a IN_PROGRESS value" do
+    expect(subject.values).to include("IN_PROGRESS")
+  end
+
+  it "is expected to define a PLANNED value" do
+    expect(subject.values).to include("PLANNED")
+  end
+
+  it "is expected to define a NOT_OPEN value" do
+    expect(subject.values).to include("NOT_OPEN")
+  end
+
+  it "is expected to define a PLANNED___SUBJECT_TO_FEASIBILITY_AND_CONSULTATION_ value" do
+    expect(subject.values).to include("PLANNED___SUBJECT_TO_FEASIBILITY_AND_CONSULTATION_")
+  end
+end
