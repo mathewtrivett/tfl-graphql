f0b28faeb663e906277c259d5a591aee80e91e99
Mathew Trivett <mathew@wearesnook.com>
2020-10-16 17:35:45 +0100

    feat: JourneyVectorType closes #3

---
+++ spec/graphql/types/journey_vector_type_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Types::JourneyVectorType do
+  subject { described_class }
+
+  it { is_expected.to have_field(:from).of_type('String') }
+  it { is_expected.to have_field(:to).of_type('String') }
+  it { is_expected.to have_field(:uri).of_type('String') }
+  it { is_expected.to have_field(:via).of_type('String') }
+end
