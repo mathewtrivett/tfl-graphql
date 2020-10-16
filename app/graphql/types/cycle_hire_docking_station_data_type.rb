bbb926df83dc1a02501ec6500c519bcdd5b99fe4
Mathew Trivett <mathew@wearesnook.com>
2020-10-16 11:47:56 +0100

    feat: Added support for CycleHireDockingStationDataType
    Closes #11

---
+++ app/graphql/types/cycle_hire_docking_station_data_type.rb
@@ -0,0 +1,10 @@
+module Types
+  class CycleHireDockingStationDataType < Types::BaseObject
+    field :destinationId, String, null: true
+    field :destinationNumberOfBikes, Integer, null: true
+    field :destinationNumberOfEmptySlots, Integer, null: true
+    field :originId, String, null: true
+    field :originNumberOfBikes, Integer, null: true
+    field :originNumberOfEmptySlots, Integer, null: true
+  end
+end
