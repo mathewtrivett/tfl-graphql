499c2bdfcb5c2e7dafbfec9f2b16710a9ca42ddc
Mathew Trivett <mathew@wearesnook.com>
2020-10-16 17:09:05 +0100

    feat: Added support and tests for RouteType closes #37

---
+++ app/graphql/types/route_type.rb
@@ -0,0 +1,12 @@
+module Types
+  class RouteType < Types::BaseEnum
+    value 'ALL'
+    value 'UNKNOWN'
+    value 'CYCLE_SUPERHIGHWAYS'
+    value 'QUIETWAYS'
+    value 'CYCLEWAYS'
+    value 'MINI_HOLLANDS'
+    value 'CENTRAL_LONDON_GRID'
+    value 'STREETSPACE_ROUTE'
+  end
+end
