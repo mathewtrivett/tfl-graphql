d2a7847187c00f00f9accd2f12ca0e19517df992
Mathew Trivett <mathew@wearesnook.com>
2020-10-18 16:40:34 +0100

    feat: Disruption type and object dependencies
    Adds support for disruption type closes #17. Adds specs and types for RouteSection type and RouteSectionNaptanEntrySequence type. Closes #29. Closes #30.

---
+++ app/graphql/types/disruption_type.rb
@@ -0,0 +1,15 @@
+module Types
+  class DisruptionType < Types::BaseObject
+    field :additionalInfo, String, null: true
+    field :affectedRoutes, [Types::RouteSectionType, null: true], null: true
+    field :affectedStops, [Types::StopPointType, null: true], null: true
+    field :category, Types::CategoryType, null: true
+    field :categoryDescription, String, null: true
+    field :closureText, String, null: true
+    field :created, String, null: true
+    field :description, String, null: true
+    field :lastUpdate, String, null: true
+    field :summary, String, null: true
+    field :type, String, null: true
+  end
+end
