5c11bb30b9ba356e3e3adf7e5ff11372059e7fe3
Mathew Trivett <mathew@wearesnook.com>
2020-10-18 16:40:34 +0100

    feat: Disruption type and object dependencies
    Adds support for disruption type closes #17. Adds specs and types for RouteSection type and RouteSectionNaptanEntrySequence type. Closes #29. Closes #30.

---
+++ app/graphql/types/route_section_type.rb
@@ -0,0 +1,15 @@
+module Types
+  class RouteSectionType < Types::BaseObject
+    field :destinationName, String, null: true
+    field :direction, String, null: true
+    field :id, String, null: true
+    field :lineId, String, null: true
+    field :lineString, String, null: true
+    field :name, String, null: true
+    field :originationName, String, null: true
+    field :routeCode, String, null: true
+    field :routeSectionNaptanEntrySequence, [Types::RouteSectionNaptanEntrySequenceType, null: true], null: true
+    field :validFrom, String, null: true
+    field :validTo, String, null: true
+  end
+end
---
+++ spec/graphql/types/route_section_type_spec.rb
@@ -0,0 +1,18 @@
+
+require 'rails_helper'
+
+describe Types::RouteSectionType do
+  subject { described_class }
+
+  it { is_expected.to have_field(:destinationName).of_type('String') }
+  it { is_expected.to have_field(:direction).of_type('String') }
+  it { is_expected.to have_field(:id).of_type('String') }
+  it { is_expected.to have_field(:lineId).of_type('String') }
+  it { is_expected.to have_field(:lineString).of_type('String') }
+  it { is_expected.to have_field(:name).of_type('String') }
+  it { is_expected.to have_field(:originationName).of_type('String') }
+  it { is_expected.to have_field(:routeCode).of_type('String') }
+  it { is_expected.to have_field(:routeSectionNaptanEntrySequence).of_type('[RouteSectionNaptanEntrySequence]') }
+  it { is_expected.to have_field(:validFrom).of_type('String') }
+  it { is_expected.to have_field(:validTo).of_type('String') }
+end
