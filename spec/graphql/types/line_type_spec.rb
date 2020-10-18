bd18860808652b6b841cf7836af656a31fcf0457
Mathew Trivett <mathew@wearesnook.com>
2020-10-18 17:33:38 +0100

    feat: Line type closes #8

---
+++ spec/graphql/types/line_type_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Types::LineType do
+  subject { described_class }
+
+  it { is_expected.to have_field(:crowding).of_type('Crowding') }
+  it { is_expected.to have_field(:disruptions).of_type('[Disruption]') }
+  it { is_expected.to have_field(:id).of_type('String') }
+  it { is_expected.to have_field(:lineStatuses).of_type('[LineStatus]') }
+  it { is_expected.to have_field(:modeName).of_type('String') }
+  it { is_expected.to have_field(:modified).of_type('String') }
+  it { is_expected.to have_field(:name).of_type('String') }
+  it { is_expected.to have_field(:routeSections).of_type('[MatchedRoute]') }
+  it { is_expected.to have_field(:serviceTypes).of_type('[LineServiceTypeInfo]') }
+end
