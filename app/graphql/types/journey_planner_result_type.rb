f42af33bb5e306516d0cc1bd4c3807a7852ee59c
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 20:16:57 +0100

    fix: Deleted extra resultTypes files

--- app/graphql/types/journey_planner_result_type.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  class JourneyPlannerResultType < Types::BaseObject
-    field :recommendedMaxAgeMinutes, Integer, null: true
-    field :stopMessages, [String, null: true], null: true
-  end
-end
