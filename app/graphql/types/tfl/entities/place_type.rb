bc70ddeefadb728a159d72c820275ea6ffe5499d
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 18:29:30 +0100

    fix: Created entities namespace for Placetype
    To mirror the Tfl api namespacing, I've created a new Entities namespace for certain types. It seems that the Entities namespace holds generic models that are general across different endpoints.

---
+++ app/graphql/types/tfl/entities/place_type.rb
@@ -0,0 +1,18 @@
+module Types
+  module Tfl
+      module Entities
+      class PlaceType < Types::BaseObject
+        field :additionalProperties, [Types::AdditionalPropertiesType, null: true], null: true
+        field :children, [Types::Tfl::Entities::PlaceType, null: true], null: true
+        field :childrenUrls, [String, null: true], null: true
+        field :commonName, String, null: true
+        field :distance, Float, null: true
+        field :id, String, null: true
+        field :lat, Float, null: true
+        field :lon, Float, null: true
+        field :placeType, String, null: true
+        field :url, String, null: true
+      end
+    end
+  end
+end
--- spec/graphql/types/tfl/entities/place_type_spec.rb
+++ spec/graphql/types/tfl/entities/place_type_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe Types::PlaceType do
+describe Types::Tfl::Entities::PlaceType do
   subject { described_class }
 
   it { is_expected.to have_field(:additionalProperties).of_type('[AdditionalProperties]') }
