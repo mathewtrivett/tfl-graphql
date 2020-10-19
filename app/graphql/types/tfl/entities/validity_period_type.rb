93efb3931a824fac300bd6fdd3ce3a9c3f62c263
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 19:16:06 +0100

    fix: Namespaced validity period

---
+++ app/graphql/types/tfl/entities/validity_period_type.rb
@@ -0,0 +1,11 @@
+module Types
+  module Tfl
+    module Entities
+      class ValidityPeriodType < Types::BaseObject
+        field :fromDate, String, null: true
+        field :isNow, Boolean, null: true
+        field :toDate, String, null: true
+      end
+    end
+  end
+end
