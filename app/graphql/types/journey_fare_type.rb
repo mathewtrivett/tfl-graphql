93e2ab3b50a7c1ef06723d96bc666c13f126ff1a
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 19:34:10 +0100

    fix: JourneyFare now namespaced

--- app/graphql/types/journey_fare_type.rb
+++
@@ -1,7 +0,0 @@
-module Types
-  class JourneyFareType < Types::BaseObject
-    field :caveats, [Types::JourneyPlannerFareCaveatType, null: true], null: true
-    field :fares, [Types::JourneyPlannerFareType, null: true], null: true
-    field :totalCost, Integer, null: true
-  end
-end
