2c6ab6b709250cdda84d3dbd8eb32f88f682ab26
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 19:43:58 +0100

    fix: namespaced faretapdetails

--- app/graphql/types/tfl/entities/journey_planner/fare_tap_type.rb
+++ app/graphql/types/tfl/entities/journey_planner/fare_tap_type.rb
@@ -4,7 +4,7 @@ module Types
       module JourneyPlanner
         class FareTapType < Types::BaseObject
           field :atcoCode, String, null: true
-          field :tapDetails, Types::FareTapDetailsType, null: true
+          field :tapDetails, Types::Tfl::Entities::JourneyPlanner::FareTapDetailsType, null: true
         end
       end
     end
