9ac4a543f52e3a95d14d1cb2e5fd74557d15fff9
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 19:59:26 +0100

    fix: Namespaced PlannedWorks

--- spec/graphql/types/tfl/entities/journey_planner/planned_work_type_spec.rb
+++ spec/graphql/types/tfl/entities/journey_planner/planned_work_type_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe Types::PlannedWorkType do
+describe Types::Tfl::Entities::JourneyPlanner::PlannedWorkType do
   subject { described_class }
 
   it { is_expected.to have_field(:createdDateTime).of_type('String') }
