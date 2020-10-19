2d9dce2e78cde5721403c37a8bf373761c1c8dcf
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 19:51:36 +0100

    fix: Namespaced instructionStep

---
+++ app/graphql/types/tfl/entities/instruction_step_type.rb
@@ -0,0 +1,21 @@
+module Types
+  module Tfl
+    module Entities
+      class InstructionStepType < Types::BaseObject
+        field :cumulativeDistance, Integer, null: true
+        field :cumulativeTravelTime, Integer, null: true
+        field :description, String, null: true
+        field :descriptionHeading, String, null: true
+        field :distance, Integer, null: true
+        field :latitude, Float, null: true
+        field :longitude, Float, null: true
+        field :pathAttribute, Types::PathAttributeType, null: true
+        field :skyDirection, Integer, null: true
+        field :skyDirectionDescription, Types::SkyDirectionDescriptionType, null: true
+        field :streetName, String, null: true
+        field :trackType, Types::TrackType, null: true
+        field :turnDirection, String, null: true
+      end
+    end
+  end
+end
--- app/graphql/types/tfl/entities/instruction_type.rb
+++ app/graphql/types/tfl/entities/instruction_type.rb
@@ -3,7 +3,7 @@ module Types
     module Entities
       class InstructionType < Types::BaseObject
         field :detailed, String, null: true
-        field :steps, [Types::InstructionStepType, null: true], null: true
+        field :steps, [Types::Tfl::Entities::InstructionStepType, null: true], null: true
         field :summary, String, null: true
       end
     end
