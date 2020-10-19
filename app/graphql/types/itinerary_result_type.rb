6bb0997d6646990173c2468e1c32ece6b1f3ab49
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 18:43:06 +0100

    fix: Namespaced linetype

--- app/graphql/types/itinerary_result_type.rb
+++ app/graphql/types/itinerary_result_type.rb
@@ -2,7 +2,7 @@ module Types
   class ItineraryResultType < Types::BaseObject
     field :stopMessages, [String, null: true], null: true
     field :recommendedMaxAgeMinutes, Integer, null: true
-    field :lines, [Types::LineType, null: true], null: true
+    field :lines, [Types::Tfl::Entities::LineType, null: true], null: true
     field :journeys, [Types::JourneyType, null: true], null: true
     field :journeyVector, Types::JourneyVectorType, null: true
     field :searchCriteria, Types::SearchCriteriaType, null: true
---
+++ app/graphql/types/tfl/entities/line_type.rb
@@ -0,0 +1,18 @@
+module Types
+  module Tfl
+    module Entities
+      class LineType < Types::BaseObject
+        field :created, String, null: true
+        field :crowding, Types::CrowdingType, null: true
+        field :disruptions, [Types::DisruptionType, null: true], null: true
+        field :id, String, null: true
+        field :lineStatuses, [Types::LineStatusType, null: true], null: true
+        field :modeName, String, null: true
+        field :modified, String, null: true
+        field :name, String, null: true
+        field :routeSections, [Types::MatchedRouteType, null: true], null: true
+        field :serviceTypes, [Types::LineServiceTypeInfoType, null: true], null: true
+      end
+    end
+  end
+end
