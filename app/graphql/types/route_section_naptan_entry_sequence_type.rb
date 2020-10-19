a7db46817991abef078626d421f6cba2d966e090
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 19:01:57 +0100

    fix: Namespace RouteSectionNaptan...

--- app/graphql/types/route_section_naptan_entry_sequence_type.rb
+++
@@ -1,6 +0,0 @@
-module Types
-  class RouteSectionNaptanEntrySequenceType < Types::BaseObject
-    field :ordinal, Integer, null: true
-    field :stopPoint, Types::StopPointType, null: true
-  end
-end
