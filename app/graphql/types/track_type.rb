f54faad7b38c919cd737a6b87baf1e6638b86ffe
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 09:42:33 +0100

    feat: TrackType closes #54

---
+++ app/graphql/types/track_type.rb
@@ -0,0 +1,12 @@
+module Types
+  class TrackType < Types::BaseEnum
+    value 'CYCLESUPERHIGHWAY'
+    value 'CANALTOWPATH'
+    value 'QUIETROAD'
+    value 'PROVISIONFORCYCLISTS'
+    value 'BUSYROADS'
+    value 'NONE'
+    value 'PUSHBIKE'
+    value 'QUIETWAY'
+  end
+end
