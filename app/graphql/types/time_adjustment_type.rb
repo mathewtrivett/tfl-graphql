61b34810e3b3889359ab5fc8618f614dbb465445
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 20:07:38 +0100

    fix: namespaced time adjustment(s)

--- app/graphql/types/time_adjustment_type.rb
+++
@@ -1,8 +0,0 @@
-module Types
-  class TimeAdjustmentType < Types::BaseObject
-    field :date, String, null: true
-    field :time, String, null: true
-    field :timeIs, String, null: true
-    field :uri, String, null: true
-  end
-end
--- app/graphql/types/time_adjustments_type.rb
+++
@@ -1,8 +0,0 @@
-module Types
-  class TimeAdjustmentsType < Types::BaseObject
-    field :earlier, Types::TimeAdjustmentType, null: true
-    field :earliest, Types::TimeAdjustmentType, null: true
-    field :later, Types::TimeAdjustmentType, null: true
-    field :latest, Types::TimeAdjustmentType, null: true
-  end
-end
