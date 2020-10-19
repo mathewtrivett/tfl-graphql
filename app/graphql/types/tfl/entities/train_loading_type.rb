9389c8c58203cb89f938cd665bf910b9d1cdabfb
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 18:53:06 +0100

    fix: Trainloading type and dependent code

---
+++ app/graphql/types/tfl/entities/train_loading_type.rb
@@ -0,0 +1,15 @@
+module Types
+  module Tfl
+    module Entities
+      class TrainLoadingType < Types::BaseObject
+        field :direction, String, null: true
+        field :line, String, null: true
+        field :lineDirection, String, null: true
+        field :naptanTo, String, null: true
+        field :platformDirection, String, null: true
+        field :timeSlice, String, null: true
+        field :value, Integer, null: true
+      end
+    end
+  end
+end
