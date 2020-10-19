90116cf741b255fcdef5daa78547ba08e15eb24a
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 09:45:57 +0100

    feat: SkyDirectionDescription closes #53

---
+++ app/graphql/types/sky_direction_description_type.rb
@@ -0,0 +1,12 @@
+module Types
+  class SkyDirectionDescriptionType < Types::BaseEnum
+    value 'NORTH'
+    value 'NORTHEAST'
+    value 'EAST'
+    value 'SOUTHEAST'
+    value 'SOUTH'
+    value 'SOUTHWEST'
+    value 'WEST'
+    value 'NORTHWEST'
+  end
+end
