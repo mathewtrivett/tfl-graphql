6e44a9ac7aa50830b07ccb58b47a7e764230bb3e
Mathew Trivett <mathew@wearesnook.com>
2020-10-19 20:02:41 +0100

    fix: Namespaced routeOption

--- app/graphql/types/route_option_type.rb
+++
@@ -1,8 +0,0 @@
-module Types
-  class RouteOptionType < Types::BaseObject
-    field :directions, [String, null: true], null: true
-    field :id, String, null: true
-    field :lineIdentifier, Types::Tfl::Entities::IdentifierType, null: true
-    field :name, String, null: true
-  end
-end
