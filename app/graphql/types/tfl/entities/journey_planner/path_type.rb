63307f0c9e6406b70ac3b19013ac4d7a70f82cc5
Mathew Trivett <mathew@wearesnook.com>
2020-10-20 09:23:43 +0100

    docs: Adds a technical decision record for namespacing
    The hope is that the namespacing approach used is intuitive but to make it explicit, I've written a architectural decision record to outline the namespacing approach used. This commit also completes namespacing by introducing the `Tfl::Common` module. Closes #46

--- app/graphql/types/tfl/entities/journey_planner/path_type.rb
+++ app/graphql/types/tfl/entities/journey_planner/path_type.rb
@@ -3,7 +3,7 @@ module Types
     module Entities
       module JourneyPlanner
         class PathType < Types::BaseObject
-          field :elevation, [Types::JpElevationType, null: true], null: true
+          field :elevation, [Types::Tfl::Common::JpElevationType, null: true], null: true
           field :lineString, String, null: true
           field :stopPoints, [Types::Tfl::Entities::IdentifierType, null: true], null: true
         end
--- docs/adr/README.md
+++ docs/adr/README.md
@@ -1,3 +1,7 @@
 # Architectural Decision Records
 
 > For recording the reasoning behind architectural decisions it's conventional to use [architectural decision records](http://thinkrelevance.com/blog/2011/11/15/documenting-architecture-decisions) in the /docs/adr directory.
+
+## Technical decisions
+
+1.  [Namespacing TfL Types](0001-namepspacing-tfl-types.md)
