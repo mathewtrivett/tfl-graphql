25496962c420735ca127f390b9bfebae42fae6a9
Mathew Trivett <mathew@wearesnook.com>
2020-10-22 17:20:57 +0100

    basic tests passing for accident-stats endpoint

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,11 +6,23 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :journey_meta_modes, [Tfl::Entities::ModeType], null: false,
-                               description: 'Gets a list of all of the available journey planner modes'
+    field :journey_meta_modes, [Tfl::Entities::ModeType], null: false do
+      description 'Gets a list of all of the available journey planner modes'
+    end
+                               
+    field :get_accident_stats, [Tfl::Entities::AccidentStats::DetailType], null: true do
+      description 'Gets all accident details for accidents occuring in the specified year'
+      argument :year, Integer, required: true
+    end
+
     def journey_meta_modes
-      @client = TflApi::Client.new(app_id: ENV['TFL_APP_ID'], app_key: ENV['TFL_APP_KEY'], host: ENV['TFL_APP_BASE'])
-      @client.journey.modes
+      @tfl = TflApi::Client.new(app_id: ENV['TFL_APP_ID'], app_key: ENV['TFL_APP_KEY'], host: ENV['TFL_APP_BASE'])
+      @tfl.journey.modes
+    end
+
+    def get_accident_stats(params)
+      @tfl = TflApi::Client.new(app_id: ENV['TFL_APP_ID'], app_key: ENV['TFL_APP_KEY'], host: ENV['TFL_APP_BASE'], log_level: 'ERROR')
+      @tfl.accident_stats.details(params[:year])
     end
   end
 end
